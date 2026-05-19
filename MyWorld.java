c2298f806d3cb84d8206a9b356eb8d530df9fc39
Jason <baifnjdn@gmail.com>
2026-05-19 11:40:35 -0400

    Started working on MyWorld

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,19 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    
+    int maxSpirits = 3;
+    Spirit[] playerSpirits;
+
     public MyWorld() {
+
         super(600, 400, 1);
+
+        playerSpirits = new Spirit[maxSpirits];
+        
+    }
+
+    public void chooseSpirit() {
+        // Code to allow the player to choose a spirit
     }
 }
