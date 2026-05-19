f7565adc81d988d4ecefc08ad27cd86cb6fde890
348831199 <348831199@gapps.yrdsb.ca>
2026-05-19 11:43:59 -0400

    attempt

--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,6 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
+        Spirit f = new Spirit("Fire", "fire spirit");
     }
 }
