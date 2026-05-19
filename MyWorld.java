df8e0431cc90f3926beec793a85f58c29347345e
348831199 <348831199@gapps.yrdsb.ca>
2026-05-19 11:49:40 -0400

    attempted things

--- MyWorld.java
+++ MyWorld.java
@@ -8,13 +8,8 @@ public class MyWorld extends World {
     public MyWorld() {
 
         super(600, 400, 1);
-<<<<<<< HEAD
-        Spirit f = new Spirit("Fire", "fire spirit");
-=======
-
         playerSpirits = new Spirit[maxSpirits];
         
->>>>>>> 3d2bd7bf353032a3eb907c0965cb73c36b5fcdde
     }
 
     public void chooseSpirit() {
