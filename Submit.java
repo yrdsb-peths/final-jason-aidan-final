35caf7a6b235c062e6e1de6354e94cdf37954270
348831199 <348831199@gapps.yrdsb.ca>
2026-05-20 11:38:37 -0400

    start game boolean variable added

--- Submit.java
+++ Submit.java
@@ -12,16 +12,18 @@ public class Submit extends Actor
      * Act - do whatever the Submit wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
+    boolean startGame;
     public Submit()
     {
         GreenfootImage image = new GreenfootImage("karo.png");
         setImage(image);
+        startGame = false;
     }
     public void act()
     {
         // Add your action code here.
-        if (Greenfoot.mouseClicked(this)) {
+        if (Greenfoot.mouseClicked(this) && !startGame) {
+            startGame = true;
             Greenfoot.stop();
         }
     }
